47930af54023a7fe0d6f113c3dfb67cba6cf8500
imhta <imhtapm@gmail.com>
2019-08-15 15:52:07 +0530

    completed non get request

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -7,3 +7,12 @@ puts "-"*20
 puts RestClient.get("http://localhost:3000/users/:id")
 puts "-"*20
 puts RestClient.get("http://localhost:3000/users/:id/edit")
+puts "-"*20
+puts RestClient.post("#{url}", " ")
+puts "-"*20
+# puts RestClient.patch("#{url}/:id", " ")
+# puts "-"*20
+# puts RestClient.put("#{url}/:id", " ")
+# puts "-"*20
+# puts RestClient.delete("#{url}/:id")
+# puts "-"*20
